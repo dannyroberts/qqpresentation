ffccf1a2f57e61ea7f6b658933666def4401d2a4
Daniel Roberts <droberts@dimagi.com>
2018-05-10 10:41:08 -0400

    add ability to skip cache

--- example.py
+++ example.py
@@ -7,9 +7,12 @@ def clear_all_posts_cache(person):
     cache.delete(cache_key)
 
 
-def get_all_posts(person):
+def get_all_posts(person, strict=False):
     cache_key = get_all_posts_cache_key(person)
-    posts = cache.get(cache_key, default=None)
+    if strict:
+        posts = None
+    else:
+        posts = cache.get(cache_key, default=None)
     if posts is None:
         posts = Post.objects.filter(person=person).all()
         cache.set(cache_key, posts)
