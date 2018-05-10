83f42697f778aa266f5cda1285367a7fac2dea0b
Daniel Roberts <droberts@dimagi.com>
2018-05-10 11:12:39 -0400

    add quickcache caching

--- quickcache_example.py
+++ quickcache_example.py
@@ -1,2 +1,8 @@
+from quickcache.django_quickcache import get_django_quickcache
+
+quickcache = get_django_quickcache(timeout=5 * 60, memoize_timeout=0)
+
+
+@quickcache(['person.id'])
 def get_all_posts(person):
     return Post.objects.filter(person=person).all()
