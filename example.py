e1f4738a85117603872632a2b74211b3ddb99ef1
Daniel Roberts <droberts@dimagi.com>
2018-05-10 10:35:21 -0400

    add caching

--- example.py
+++ example.py
@@ -1,2 +1,7 @@
 def get_all_posts(person):
-        return Post.objects.filter(person=person).all()
+    cache_key = 'get_all_posts:{}'.format(person.id)
+    posts = cache.get(cache_key, default=None)
+    if posts is None:
+        posts = Post.objects.filter(person=person).all()
+        cache.set(cache_key, posts)
+    return posts
