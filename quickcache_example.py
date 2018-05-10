6449a8c7ea796e6cdcdd2c722e2aca5bd4596e6f
Daniel Roberts <droberts@dimagi.com>
2018-05-10 11:26:49 -0400

    add timeout

--- quickcache_example.py
+++ quickcache_example.py
@@ -3,6 +3,6 @@ from quickcache.django_quickcache import get_django_quickcache
 quickcache = get_django_quickcache(timeout=5 * 60, memoize_timeout=0)
 
 
-@quickcache(['person.id'], skip_arg='strict')
+@quickcache(['person.id'], skip_arg='strict', timeout=60 * 60)
 def get_all_posts(person, strict=False):
     return Post.objects.filter(person=person).all()
