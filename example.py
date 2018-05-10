1b2f6f80ff49a840830bd9f759fc461932f92e0b
Daniel Roberts <droberts@dimagi.com>
2018-05-10 10:39:47 -0400

    add cache invalidation

--- example.py
+++ example.py
@@ -2,6 +2,11 @@ def get_all_posts_cache_key(person):
     return 'get_all_posts:{}'.format(person.id)
 
 
+def clear_all_posts_cache(person):
+    cache_key = get_all_posts_cache_key(person)
+    cache.delete(cache_key)
+
+
 def get_all_posts(person):
     cache_key = get_all_posts_cache_key(person)
     posts = cache.get(cache_key, default=None)
