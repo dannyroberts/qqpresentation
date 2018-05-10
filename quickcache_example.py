f27fc629b30685bccaac3f016340d66237def27a
Daniel Roberts <droberts@dimagi.com>
2018-05-10 11:26:19 -0400

    add quickcache skip arg

--- quickcache_example.py
+++ quickcache_example.py
@@ -3,6 +3,6 @@ from quickcache.django_quickcache import get_django_quickcache
 quickcache = get_django_quickcache(timeout=5 * 60, memoize_timeout=0)
 
 
-@quickcache(['person.id'])
-def get_all_posts(person):
+@quickcache(['person.id'], skip_arg='strict')
+def get_all_posts(person, strict=False):
     return Post.objects.filter(person=person).all()
